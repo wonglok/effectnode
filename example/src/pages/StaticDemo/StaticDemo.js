7629250f3c0fca6cecaacc36d7521f8e1cf3cbff
wonglok <yellowhappy831@gmail.com>
2021-07-31 13:06:09 +0800

    save

--- example/src/pages/StaticDemo/StaticDemo.js
+++ example/src/pages/StaticDemo/StaticDemo.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { ENRuntime, processRawData } from 'effectnode'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { getRawFirebaseData } from './getRawFirebaseData'
 
 export function StaticDemo() {
   return (
@@ -21,7 +22,7 @@ export function EffectNodeInFiber() {
 
   useEffect(() => {
     graph.current = new ENRuntime({
-      json: processRawData(getRawJSON()),
+      json: processRawData(getRawFirebaseData()),
       codes: getCodes()
     })
 
@@ -77,152 +78,6 @@ export const getCodes = () => {
   let codes = importAll(r)
 
   return codes
-}
-
-export const getRawJSON = () => {
-  return {
-    NGpUixuU0NOkOlmLsLuepkaZxxt1: {
-      connections: {
-        '-MdPINDxi9T3AotCKSfB': {
-          _id: '_via641l4y3wgloaeb7',
-          input: {
-            _id: '_od5ohzl5avwo3ltnf8',
-            nodeID: '_lka05oa2emaxqkxd7w',
-            type: 'input'
-          },
-          output: {
-            _id: '_iys0bi1wbszoajdrbq',
-            nodeID: '_sbhxwatnnvkk95yslm',
-            type: 'output'
-          }
-        }
-      },
-      nodes: {
-        '-MdFPnHiFrg5AXKZPPLV': {
-          _id: '_lka05oa2emaxqkxd7w',
-          inputs: [
-            {
-              _id: '_od5ohzl5avwo3ltnf8',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'input'
-            },
-            {
-              _id: '_fbb9aoyc66f0f057p4',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'input'
-            },
-            {
-              _id: '_kbb8xyxn4zegnkmww4',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'input'
-            },
-            {
-              _id: '_j27f14qjt3crj7jrkm',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'input'
-            },
-            {
-              _id: '_5awgse7wgryzl4unfr',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'input'
-            }
-          ],
-          outputs: [
-            {
-              _id: '_wcz630splzoznvol9b',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'output'
-            },
-            {
-              _id: '_yn1yu9pye1gep7hui2',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'output'
-            },
-            {
-              _id: '_aydf7dn80m375qhcus',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'output'
-            },
-            {
-              _id: '_p9k078jvmtv2muzw8t',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'output'
-            },
-            {
-              _id: '_g4d8r8w1y5ybgdutey',
-              nodeID: '_lka05oa2emaxqkxd7w',
-              type: 'output'
-            }
-          ],
-          position: [
-            13.81351241290946, -6.268112984381583e-15, 28.229071300777065
-          ],
-          title: 'lok.mySecondNode.js'
-        },
-        '-MdMk9rFgJSESwDybIfB': {
-          _id: '_sbhxwatnnvkk95yslm',
-          inputs: [
-            {
-              _id: '_dzsph16c3ubtltaukb',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'input'
-            },
-            {
-              _id: '_8oxaioc4n4xche1mmi',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'input'
-            },
-            {
-              _id: '_h67wqxjauc8uit6qzf',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'input'
-            },
-            {
-              _id: '_txfnifvgobsqp7wkno',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'input'
-            },
-            {
-              _id: '_hh7yqw6u0kaa2f4vsr',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'input'
-            }
-          ],
-          outputs: [
-            {
-              _id: '_iys0bi1wbszoajdrbq',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'output'
-            },
-            {
-              _id: '_s06csj99keboy364wd',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'output'
-            },
-            {
-              _id: '_r94f0g30633kompktd',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'output'
-            },
-            {
-              _id: '_lm15qdzn3k08zt783f',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'output'
-            },
-            {
-              _id: '_izp3hqkt018y23lwk0',
-              nodeID: '_sbhxwatnnvkk95yslm',
-              type: 'output'
-            }
-          ],
-          position: [
-            23.8559458588784, 1.363425432176458e-15, -6.1403222682973535
-          ],
-          title: 'lok.myFristNode.js'
-        }
-      }
-    }
-  }
 }
 
 if (module.hot) {
