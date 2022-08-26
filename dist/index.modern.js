e585c9fdc110f7774a2053ac994a79e6be25bfb8
Wong Lok <wonglok@users.noreply.github.com>
2022-08-27 06:38:38 +0800

    yo

--- dist/index.modern.js
+++
@@ -1,9 +0,0 @@
-import { Box } from '@react-three/drei';
-import React from 'react';
-
-function EffectNode(_ref) {
-  return /*#__PURE__*/React.createElement("group", null, /*#__PURE__*/React.createElement(Box, null));
-}
-
-export { EffectNode };
-//# sourceMappingURL=index.modern.js.map
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import { Canvas } from '@react-three/fiber'
-import { EffectNode } from '../src/Runtime/EffectNode'
+import { Suspense } from 'react'
+import { EffectNode } from '@/index'
 
 export default function Render() {
   return (
     <div className='w-full h-full'>
       <Canvas>
-        <EffectNode></EffectNode>
+        <Suspense fallback={null}>
+          <Content></Content>
+        </Suspense>
       </Canvas>
     </div>
   )
 }
+
+function Content() {
+  return (
+    <group>
+      <EffectNode></EffectNode>
+    </group>
+  )
+}
+
+//
+//
