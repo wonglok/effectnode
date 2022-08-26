e5a12b556cc0ec42e4c53145620fad7dc909d6d1
Wong Lok <wonglok@users.noreply.github.com>
2022-08-26 23:31:23 +0800

    save

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+function EffectNode() {
+  return /*#__PURE__*/React.createElement("group", null);
+}
+
+exports.EffectNode = EffectNode;
+//# sourceMappingURL=index.js.map
---
+++ dist/index.modern.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function EffectNode() {
+  return /*#__PURE__*/React.createElement("group", null);
+}
+
+export { EffectNode };
+//# sourceMappingURL=index.modern.js.map
