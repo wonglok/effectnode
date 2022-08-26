ddd1570b1723cc8c570fd98f02a1c7daafde07d4
Wong Lok <wonglok@users.noreply.github.com>
2022-08-26 23:50:33 +0800

    yo

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import { Canvas } from '@react-three/fiber'
 import { EffectNode } from '../src/Runtime/EffectNode'
+
 export default function Render() {
   return (
-    <div>
-      <EffectNode></EffectNode>
+    <div className='w-full h-full'>
+      <Canvas>
+        <EffectNode></EffectNode>
+      </Canvas>
     </div>
   )
 }
